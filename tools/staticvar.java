7418f92a3ac39c1f13d6999269339e55918e52e9
sindaarous <58075035+sindaarous@users.noreply.github.com>
2021-04-29 02:18:09 +0200

    Add files via upload

---
+++ tools/staticvar.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esprit.recrutini.tools;
+
+/**
+ *
+ * @author asus
+ */
+public class staticvar {
+   public static String SALT = "$2a$10$OYty/sJuxGPcgxtIKQHiWe"; 
+}
